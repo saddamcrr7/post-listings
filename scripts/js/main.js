15a9f412b2722421ec075148f58245ee0b835f1e
Hossain Saddam <hossaincrr7@gmail.com>
2020-01-20 01:58:03 +0600

    update product view container

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,44 +1,47 @@
 "use strict";
 
 var selected = document.querySelectorAll('.c-input-select');
-selected.forEach(function (element) {
-  var selectedText = element.querySelector('.c-input-select__selected-text');
-  var options = element.querySelector('.c-input-select__options');
-  var optionItems = options.querySelectorAll('.c-input-select__options-item');
-  var filterIcon = element.querySelector('.c-input-select__icon');
-  var optionsOpen = 0;
+if (selected) {
+  selected.forEach(function (element) {
+    var selectedText = element.querySelector('.c-input-select__selected-text');
+    var options = element.querySelector('.c-input-select__options');
+    var optionItems = options.querySelectorAll('.c-input-select__options-item');
+    var filterIcon = element.querySelector('.c-input-select__icon');
+    var optionsOpen = 0;
 
-  function toggleOptions() {
-    var optionsHeight;
-    optionItems.forEach(function (item) {
-      return optionsHeight = item.offsetHeight * optionItems.length;
-    });
+    function toggleOptions() {
+      var optionsHeight;
+      optionItems.forEach(function (item) {
+        return optionsHeight = item.offsetHeight * optionItems.length;
+      });
 
-    if (optionsOpen == 0) {
-      options.style.height = "".concat(optionsHeight, "px");
-      options.style.bottom = "-".concat(optionsHeight, "px");
-      filterIcon.style.transform = "rotate(-180deg) translateY(50%)";
-      optionsOpen = !0;
-    } else {
-      options.style.height = "0px";
-      filterIcon.style.transform = "rotate(0deg) translateY(-50%)";
-      optionsOpen = 0;
+      if (optionsOpen == 0) {
+        options.style.height = "".concat(optionsHeight, "px");
+        options.style.bottom = "-".concat(optionsHeight, "px");
+        filterIcon.style.transform = "rotate(-180deg) translateY(50%)";
+        optionsOpen = !0;
+      } else {
+        options.style.height = "0px";
+        filterIcon.style.transform = "rotate(0deg) translateY(-50%)";
+        optionsOpen = 0;
+      }
     }
-  }
 
-  element.addEventListener('click', function () {
-    return toggleOptions();
-  });
-  optionItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-      toggleOptions();
-      if (element.dataset.value == item.dataset.value) return;
-      toggleOptions();
-      element.dataset.value = item.dataset.value;
-      selectedText.innerText = item.innerText;
+    element.addEventListener('click', function () {
+      return toggleOptions();
+    });
+    optionItems.forEach(function (item) {
+      item.addEventListener('click', function () {
+        toggleOptions();
+        if (element.dataset.value == item.dataset.value) return;
+        toggleOptions();
+        element.dataset.value = item.dataset.value;
+        selectedText.innerText = item.innerText;
+      });
     });
   });
-});
+}
+
 
 var videosSliderTrendingCreators = new Swiper('.o-products-slider__container', {
   slidesPerView: 6,
@@ -47,8 +50,8 @@ var videosSliderTrendingCreators = new Swiper('.o-products-slider__container', {
   slideClass: 'o-products-slider__item ',
   slideActiveClass: 'o-products-slider__item--slide-active',
   navigation: {
-      nextEl: '.o-products-slider__arrow--next',
-      prevEl: '.o-products-slider__arrow--prev',
+    nextEl: '.o-products-slider__arrow--next',
+    prevEl: '.o-products-slider__arrow--prev',
   },
   breakpoints: {
     1200: {
@@ -57,3 +60,33 @@ var videosSliderTrendingCreators = new Swiper('.o-products-slider__container', {
     },
   },
 })
+
+
+var productView = document.querySelector('.o-product-view');
+var productViewContianer = productView.querySelector('.o-product-view__container');
+
+if (productView) {
+  var btnList = productView.querySelector('.o-product-view__style-list');
+  var btnGrid = productView.querySelector('.o-product-view__style-grid');
+  var products = productView.querySelectorAll('.c-product');
+
+  btnList.addEventListener('click', function () {
+    products = productView.querySelectorAll('.c-product');
+    if (!products) return;
+    products.forEach(function (product) {
+      product.classList.remove('c-product--medium');
+      product.classList.add('c-product--wide');
+      productViewContianer.classList.remove('o-product-showcase__row');
+    });
+  });
+  
+  btnGrid.addEventListener('click', function () {
+    products = productView.querySelectorAll('.c-product');
+    if (!products) return;
+    products.forEach(function (product) {
+      productViewContianer.classList.add('o-product-showcase__row');
+      product.classList.remove('c-product--wide');
+      product.classList.add('c-product--medium');
+    });
+  });
+}
